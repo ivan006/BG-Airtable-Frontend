8860871d8b8a662009731e17dda5a0496a345e48
ivan006 <ivan.copeland2015@gmail.com>
2023-02-18 17:38:37 +0200

    1

--- src/components/AppNavigation.vue
+++ src/components/AppNavigation.vue
@@ -9,31 +9,31 @@
     >
       <v-list>
         <template v-for="(item, index) in items">
-          <v-list-tile :key="index">
-            <v-list-tile-content>
+          <v-list-item :key="index">
+            <v-list-item-content>
               {{ item.title }}
-            </v-list-tile-content>
-          </v-list-tile>
+            </v-list-item-content>
+          </v-list-item>
           <v-divider :key="`divider-${index}`"></v-divider>
         </template>
       </v-list>
     </v-navigation-drawer>
-    <v-toolbar app color="brown darken-4" dark>
-      <v-toolbar-side-icon
+    <v-app-bar app color="brown darken-4" dark>
+      <v-app-bar-nav-icon
         class="hidden-md-and-up"
         @click="drawer = !drawer"
-      ></v-toolbar-side-icon>
+      ></v-app-bar-nav-icon>
       <v-spacer class="hidden-md-and-up"></v-spacer>
       <router-link to="/">
         <v-toolbar-title to="/">{{ appTitle }}</v-toolbar-title>
       </router-link>
-      <v-btn flat class="hidden-sm-and-down" to="/menu">Menu</v-btn>
+      <v-btn text class="hidden-sm-and-down" to="/menu">Menu</v-btn>
       <v-spacer class="hidden-sm-and-down"></v-spacer>
-      <v-btn flat class="hidden-sm-and-down" to="/sign-in">SIGN IN</v-btn>
+      <v-btn text class="hidden-sm-and-down" to="/sign-in">SIGN IN</v-btn>
       <v-btn color="brown lighten-3" class="hidden-sm-and-down" to="/join"
         >JOIN</v-btn
       >
-    </v-toolbar>
+    </v-app-bar>
   </span>
 </template>
 <script>
